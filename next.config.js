bb5691409e280ed6f3827934cac5211cd4fb29d3
TheRealKrishna <agarwl.krishna2@gmail.com>
2024-02-08 15:30:58 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    swcMinify: true,
     images: {
-        domains:["fakestoreapi.com","i.dummyjson.com"]
+        domains:["fakestoreapi.com", "i.dummyjson.com", "localhost", "krishna.lol", "fakestore.krishna.lol"],
+        unoptimized:true,
     },
     compiler: {
         // Enables the styled-components SWC transform
